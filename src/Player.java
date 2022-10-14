95899513604636c11cfa22e2dc359b0bea0336a6
Aeshus <87614336+Aeshus@users.noreply.github.com>
2022-10-14 13:18:48 -0500

    Impl MVP for Player
    No comments yet, but the basic functionality is there.
    The main thing is nextLine() and nextInt() which are called from TextAdventure for user input.

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,33 @@
 public class Player {
+    private String name;
+    private int monstersDefeated;
+    private Console console;
+
+    public Player(String playerName, Console terminal) {
+        name = playerName; console = terminal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String nextLine() {
+        System.out.println("\n" + name.toUpperCase() + " (string): ");
+        String str = console.getInput();
+        System.out.println("\n-~-~-~-~-~-~-~\n");
+
+        return str;
+    }
+
+    public int nextInt() {
+        System.out.println("\n" + name.toUpperCase() + " (int): ");
+        int str = Integer.parseInt(console.getInput());
+        System.out.println("\n-~-~-~-~-~-~-~\n");
+
+        return str;
+    }
 }
